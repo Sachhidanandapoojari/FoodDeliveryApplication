4a192615955f16731b7e5152d65ee3b2395a7ec9
Sachhidanandapoojari <157864370+Sachhidanandapoojari@users.noreply.github.com>
2025-01-28 10:10:14 +0530

    Update Order.java

--- FoodDeliveryApplication9/src/main/java/com/food/model/Order.java
+++ FoodDeliveryApplication9/src/main/java/com/food/model/Order.java
@@ -9,4 +9,52 @@ public class Order {
      private double totalAmount;
      private String status;
      private String paymentMethod;
+
+     public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	public int getRestaurantId() {
+		return restaurantId;
+	}
+
+	public void setRestaurantId(int restaurantId) {
+		this.restaurantId = restaurantId;
+	}
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public double getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
 }
