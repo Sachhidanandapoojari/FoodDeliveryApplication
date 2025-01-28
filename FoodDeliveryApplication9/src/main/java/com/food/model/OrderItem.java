7b5a717d306082cc769a9738e5b8d010df737d4b
Sachhidanandapoojari <157864370+Sachhidanandapoojari@users.noreply.github.com>
2025-01-28 10:11:51 +0530

    Update OrderItem.java

--- FoodDeliveryApplication9/src/main/java/com/food/model/OrderItem.java
+++ FoodDeliveryApplication9/src/main/java/com/food/model/OrderItem.java
@@ -6,4 +6,44 @@ public class OrderItem {
      private int menuId;
      private int quantity;
      private double itemTotal;
+
+     public int getOrderItemId() {
+		return orderItemId;
+	}
+
+	public void setOrderItemId(int orderItemId) {
+		this.orderItemId = orderItemId;
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	public int getMenuId() {
+		return menuId;
+	}
+
+	public void setMenuId(int menuId) {
+		this.menuId = menuId;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getItemTotal() {
+		return itemTotal;
+	}
+
+	public void setItemTotal(double itemTotal) {
+		this.itemTotal = itemTotal;
+	}
 }
