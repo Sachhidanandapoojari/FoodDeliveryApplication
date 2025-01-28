6cb72a521710c81b10e2bf61d92132d9b51a3023
Sachhidanandapoojari <157864370+Sachhidanandapoojari@users.noreply.github.com>
2025-01-28 10:11:08 +0530

    Update OrderHistory.java

--- FoodDeliveryApplication9/src/main/java/com/food/model/OrderHistory.java
+++ FoodDeliveryApplication9/src/main/java/com/food/model/OrderHistory.java
@@ -9,4 +9,52 @@ public class OrderHistory {
        private Date orderDate;
        private double totalAmount;
        private String status;
+
+       public int getOrderHistoryId() {
+		return orderHistoryId;
+	}
+
+	public void setOrderHistoryId(int orderHistoryId) {
+		this.orderHistoryId = orderHistoryId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public double getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
 }
